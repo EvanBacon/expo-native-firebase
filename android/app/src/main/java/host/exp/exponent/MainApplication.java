0368f97653ba9c70d6975564c5d8c574b8b5d5ed
Evan Bacon <baconbrix@gmail.com>
2018-01-11 15:31:43 -0800

    removed firebase

--- android/app/src/main/java/host/exp/exponent/MainApplication.java
+++ android/app/src/main/java/host/exp/exponent/MainApplication.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 // Needed for `react-native link`
 // import com.facebook.react.ReactApplication;
-import io.invertase.firebase.RNFirebasePackage;
 
 public class MainApplication extends ExpoApplication {
 
@@ -24,8 +23,7 @@ public class MainApplication extends ExpoApplication {
         // TODO: add native modules!
 
         // Needed for `react-native link`
-        // new MainReactPackage(),
-            new RNFirebasePackage()
+        // new MainReactPackage()
     );
   }
 
